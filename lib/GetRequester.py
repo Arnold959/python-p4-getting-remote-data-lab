7d93af56554fbc4e214b21541591519c669a1325
arnold <musaliokello@gmail.com>
2023-06-22 17:32:44 +0300

    Completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,27 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        try:
+            json_data = json.loads(response_body)
+        except ValueError:
+            # If the response is not valid JSON
+            raise ValueError("Invalid JSON response")
+
+        return json_data
+
+
+URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+data = GetRequester(URL)
+converted_data = data.load_json()
+
+for item in converted_data:
+    print(item)
